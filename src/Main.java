649f428640899a10f9ce5b75f4fd0121342cd1be
Dev <subbuchikkala3@gmail.com>
2025-05-13 14:43:57 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in)
-                ;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        int sum=0;
+        for(int i=0;i<n;i++){
+
+            System.out.println(i);
+        }
+
     }
 }
