67766bc7c561a883f5fcba384c88d40279eeb7a6
Dev <subbuchikkala3@gmail.com>
2025-05-20 15:00:45 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        
-        int sum=0;
-        for(int i=0;i<n;i++){
+        int sum =0;
+        int temp=n;
+        while(temp>0){
 
-            System.out.println(i);
         }
 
     }
